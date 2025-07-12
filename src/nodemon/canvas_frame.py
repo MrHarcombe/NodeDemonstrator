9f0ef59033a8ac0e5885a874b646f95d2db0ca1e
MrHarcombe <ian.harcombe@gmail.com>
2025-07-12 08:48:48 +0000

    Debugging the node deletion - seems to be going wrong :(

--- src/nodemon/canvas_frame.py
+++ src/nodemon/canvas_frame.py
@@ -922,6 +922,13 @@ class CanvasFrame(ttk.Frame):
 
             if len(possible) != 0:
                 selected = self.__find_associated_ids(possible)
+
+                # DEBUG: print out the associated id/labels of the node the user has doouble-clicked to delete
+                for tagOrId in selected:
+                    print(
+                        f"Deleting: {tagOrId} / {', '.join(self.__canvas.gettags(tagOrId))}"
+                    )
+
                 node_loopback = None
                 node_label = None
 
@@ -938,7 +945,12 @@ class CanvasFrame(ttk.Frame):
                 if node_label is None:
                     node_label = node_loopback
 
-                if dialogs.Messagebox.yesno(message=f"Are you sure you want to delete '{node_label}'?") == "Yes":
+                if (
+                    dialogs.Messagebox.yesno(
+                        message=f"Are you sure you want to delete '{node_label}'?"
+                    )
+                    == "Yes"
+                ):
                     StateModel().delete_node(node_loopback)
                     for id in selected:
                         self.__canvas.delete(id)
@@ -967,13 +979,23 @@ class CanvasFrame(ttk.Frame):
                             node_loopback = tag[14:]
 
             if node_loopback is not None:
-                if dialogs.Messagebox.yesno(message=f"Are you sure you want to delete the loopback edge on '{node_loopback}'?") == "Yes":
+                if (
+                    dialogs.Messagebox.yesno(
+                        message=f"Are you sure you want to delete the loopback edge on '{node_loopback}'?"
+                    )
+                    == "Yes"
+                ):
                     StateModel().delete_edge(node_loopback, node_loopback)
                     self.__canvas.delete(id)
                     self.__canvas.delete(f"cost_loopback_{node_loopback}")
 
             else:
-                if dialogs.Messagebox.yesno(message=f"Are you sure you want to delete the edge from '{node_from}' to '{node_to}'?") == "Yes":
+                if (
+                    dialogs.Messagebox.yesno(
+                        message=f"Are you sure you want to delete the edge from '{node_from}' to '{node_to}'?"
+                    )
+                    == "Yes"
+                ):
                     StateModel().delete_edge(node_from, node_to)
                     self.__canvas.delete(id)
                     self.__canvas.delete(f"cost_fromto_{node_from}_{node_to}")
