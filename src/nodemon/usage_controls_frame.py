c8213e7f03709bf1f429b9e54ef773764bb5007b
Ian Harcombe <ian.harcombe@gmail.com>
2025-07-16 16:38:13 +0100

    Dialog box corrections
    Further misunderstandings over usage of ttkbootstrap dialog boxes

--- src/nodemon/usage_controls_frame.py
+++ src/nodemon/usage_controls_frame.py
@@ -26,15 +26,20 @@ class UsageControlsFrame(ttk.Frame):
     TREE_ALGOCHOICES = UNWEIGHTED_ALGOCHOICES + [
         "Pre Order Traversal",
         "In Order Traversal",
-        "Post Order Traversal"
+        "Post Order Traversal",
     ]
 
     def __init__(self, parent, canvas_frame):
         super().__init__(parent)
-        self.bind("<Expose>", lambda event: StateModel().set_current_tab(self.__class__.__name__))
+        self.bind(
+            "<Expose>",
+            lambda event: StateModel().set_current_tab(self.__class__.__name__),
+        )
 
         self.__canvas_frame = canvas_frame
-        self.__algochoice = ttk.StringVar(value=UsageControlsFrame.UNWEIGHTED_ALGOCHOICES[0])
+        self.__algochoice = ttk.StringVar(
+            value=UsageControlsFrame.UNWEIGHTED_ALGOCHOICES[0]
+        )
         self.__from = ttk.StringVar(name="FROM_VAR")
         self.__to = ttk.StringVar(name="TO_VAR")
         self.__speed = ttk.IntVar(value=5)
@@ -44,17 +49,21 @@ class UsageControlsFrame(ttk.Frame):
         # self.__from.trace_add("write", self.__capitalise_combo)
         # self.__to.trace_add("write", self.__capitalise_combo)
 
-        algo_combo = ttk.Combobox(self, values=[], textvariable=self.__algochoice, state="readonly")
-        algo_combo.grid(sticky=tk.NSEW,pady=(0, 15))
+        algo_combo = ttk.Combobox(
+            self, values=[], textvariable=self.__algochoice, state="readonly"
+        )
+        algo_combo.grid(sticky=tk.NSEW, pady=(0, 15))
         algo_combo.bind(
             "<Expose>",
             lambda event: algo_combo.configure(
                 values=(
                     UsageControlsFrame.TREE_ALGOCHOICES
                     if StateModel().is_tree()
-                    else UsageControlsFrame.WEIGHTED_ALGOCHOICES
-                    if StateModel().is_weighted()
-                    else UsageControlsFrame.UNWEIGHTED_ALGOCHOICES
+                    else (
+                        UsageControlsFrame.WEIGHTED_ALGOCHOICES
+                        if StateModel().is_weighted()
+                        else UsageControlsFrame.UNWEIGHTED_ALGOCHOICES
+                    )
                 )
             ),
         )
@@ -68,22 +77,46 @@ class UsageControlsFrame(ttk.Frame):
             column=0,
             padx=(0, 3),
         )
-        from_combo = ttk.Combobox(nodes_frame, textvariable=self.__from, values=self.__canvas_frame.get_node_labels(), state="readonly")
+        from_combo = ttk.Combobox(
+            nodes_frame,
+            textvariable=self.__from,
+            values=self.__canvas_frame.get_node_labels(),
+            state="readonly",
+        )
         from_combo.grid(row=2, column=1, sticky=tk.NSEW, pady=(0, 3))
-        from_combo.bind("<Expose>", lambda event: from_combo.configure(values=self.__canvas_frame.get_node_labels()))
+        from_combo.bind(
+            "<Expose>",
+            lambda event: from_combo.configure(
+                values=self.__canvas_frame.get_node_labels()
+            ),
+        )
 
         ttk.Label(nodes_frame, text="To:", anchor=tk.E).grid(
             row=3,
             column=0,
             padx=(0, 3),
         )
-        to_combo = ttk.Combobox(nodes_frame, textvariable=self.__to, values=self.__canvas_frame.get_node_labels(), state="readonly")
+        to_combo = ttk.Combobox(
+            nodes_frame,
+            textvariable=self.__to,
+            values=self.__canvas_frame.get_node_labels(),
+            state="readonly",
+        )
         to_combo.grid(row=3, column=1, sticky=tk.NSEW)
-        to_combo.bind("<Expose>", lambda event: to_combo.configure(values=self.__canvas_frame.get_node_labels()))
+        to_combo.bind(
+            "<Expose>",
+            lambda event: to_combo.configure(
+                values=self.__canvas_frame.get_node_labels()
+            ),
+        )
 
-        self.__step_button = ttk.Button(self, text="Step Trace", command=self.__trace_step)
+        self.__step_button = ttk.Button(
+            self, text="Step Trace", command=self.__trace_step
+        )
         self.__step_button.grid(sticky=tk.NSEW, pady=(15, 3))
-        self.__timed_button = ttk.Button(self, text="Timed Trace", command=self.__trace_timed)
+        self.__timed_button = ttk.Button(
+            self, text="Timed Trace", command=self.__trace_timed
+        )
         self.__timed_button.grid(sticky=tk.NSEW, pady=(3, 3))
         self.__timed_speed = ttk.Scale(self, from_=1, to=5, variable=self.__speed)
         self.__timed_speed.grid(sticky=tk.NSEW, pady=(3, 15))
@@ -104,7 +137,9 @@ class UsageControlsFrame(ttk.Frame):
             message = None
 
             if from_given and not from_good and to_given and not to_good:
-                message = '"From" and "To" nodes not taken from the graph drawn on screen'
+                message = (
+                    '"From" and "To" nodes not taken from the graph drawn on screen'
+                )
 
             elif from_given and not from_good:
                 message = '"From" node not taken from the graph drawn on screen'
@@ -113,15 +148,17 @@ class UsageControlsFrame(ttk.Frame):
                 message = '"To" node not taken from the graph drawn on screen'
 
             if message is not None:
-                dialogs.show_error(title="Unknown Node", message=message)
+                dialogs.Messagebox.show_error(title="Unknown Node", message=message)
                 return
 
         match self.__algochoice.get():
             case "Breadth First":
                 if from_given:
-                    self.__trace_frame = BreadthFirstFrame(self, self.__canvas_frame, from_node, to_node)
+                    self.__trace_frame = BreadthFirstFrame(
+                        self, self.__canvas_frame, from_node, to_node
+                    )
                 else:
-                    dialogs.show_error(
+                    dialogs.Messagebox.show_error(
                         title="Required Node",
                         message=f'At least the "From" node is required to trace breadth first '
                         f'{"search" if to_given else "traversal"}',
@@ -129,9 +166,11 @@ class UsageControlsFrame(ttk.Frame):
 
             case "Depth First":
                 if from_given:
-                    self.__trace_frame = DepthFirstFrame(self, self.__canvas_frame, from_node, to_node)
+                    self.__trace_frame = DepthFirstFrame(
+                        self, self.__canvas_frame, from_node, to_node
+                    )
                 else:
-                    dialogs.show_error(
+                    dialogs.Messagebox.show_error(
                         title="Required Node",
                         message=f'At least the "From" node is required to trace depth first '
                         f'{"search" if to_given else "traversal"}',
@@ -139,45 +178,55 @@ class UsageControlsFrame(ttk.Frame):
 
             case "Dijkstra's Shortest Path":
                 if from_given:
-                    self.__trace_frame = DijkstraShortestPathFrame(self, self.__canvas_frame, from_node, to_node)
+                    self.__trace_frame = DijkstraShortestPathFrame(
+                        self, self.__canvas_frame, from_node, to_node
+                    )
                 else:
-                    dialogs.show_error(
+                    dialogs.Messagebox.show_error(
                         title="Required Node",
                         message='At least the "From" node is required to trace Dijkstra\'s shortest path',
                     )
 
             case "A* Shortest Path":
                 if from_given and to_given:
-                    self.__trace_frame = AStarShortestPathFrame(self, self.__canvas_frame, from_node, to_node)
+                    self.__trace_frame = AStarShortestPathFrame(
+                        self, self.__canvas_frame, from_node, to_node
+                    )
                 else:
-                    dialogs.show_error(
+                    dialogs.Messagebox.show_error(
                         title="Required Nodes",
                         message='Both "From" and "To" nodes are required to trace A* shortest path',
                     )
 
             case "Pre Order Traversal":
                 if from_given:
-                    self.__trace_frame = TreeTraversalFrame(self, self.__canvas_frame, "Pre", from_node, to_node)
+                    self.__trace_frame = TreeTraversalFrame(
+                        self, self.__canvas_frame, "Pre", from_node, to_node
+                    )
                 else:
-                    dialogs.show_error(
+                    dialogs.Messagebox.show_error(
                         title="Required Node",
                         message='At least the "From" node is required to trace a pre-order tree traversal',
                     )
 
             case "In Order Traversal":
                 if from_given:
-                    self.__trace_frame = TreeTraversalFrame(self, self.__canvas_frame, "In", from_node, to_node)
+                    self.__trace_frame = TreeTraversalFrame(
+                        self, self.__canvas_frame, "In", from_node, to_node
+                    )
                 else:
-                    dialogs.show_error(
+                    dialogs.Messagebox.show_error(
                         title="Required Node",
                         message='At least the "From" node is required to trace a in-order tree traversal',
                     )
 
             case "Post Order Traversal":
                 if from_given:
-                    self.__trace_frame = TreeTraversalFrame(self, self.__canvas_frame, "Post", from_node, to_node)
+                    self.__trace_frame = TreeTraversalFrame(
+                        self, self.__canvas_frame, "Post", from_node, to_node
+                    )
                 else:
-                    dialogs.show_error(
+                    dialogs.Messagebox.show_error(
                         title="Required Node",
                         message='At least the "From" node is required to trace a post-order tree traversal',
                     )
@@ -205,14 +254,18 @@ class UsageControlsFrame(ttk.Frame):
 
         def resume_step():
             interval = self.__speed.get() * 0.5  # half a second per blip
-            self.__timer_token = self.__timed_button.after(int(interval * 1000), setup_next_step)
+            self.__timer_token = self.__timed_button.after(
+                int(interval * 1000), setup_next_step
+            )
             self.__timed_button.configure(text="Pause")
             self.__timed_button.configure(command=pause_step)
 
         def setup_next_step():
             if self.__trace_frame.step():
                 interval = self.__speed.get() * 0.5  # half a second per blip
-                self.__timer_token = self.__timed_button.after(int(interval * 1000), setup_next_step)
+                self.__timer_token = self.__timed_button.after(
+                    int(interval * 1000), setup_next_step
+                )
 
         if self.__create_trace_frame():
             self.__step_button.configure(state=tk.DISABLED)
